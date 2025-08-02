a39a7424b2289072f25302f05bf8b5915d7ee0ba
Artem <rapid9791@gmail.com>
2025-08-02 20:19:57 +0300

    feat: Added lifespan to manage application lifecycle

--- src/app.py
+++ src/app.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
-from src.router.api import api_router
+from src._router.api import api_router
+from src.database import init_db
+# import uvicorn
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(api_router)
 
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 # if __name__ == "__main__":
 #     uvicorn.run(app, host="0.0.0.0", port=8000)
