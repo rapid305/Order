9f35803311e481dae549f256c950d98606160aa4
Artem <rapid9791@gmail.com>
2025-08-03 01:37:29 +0300

    feat: add pagination+filters to orders GET

--- src/routs/orderRoute.py
+++ src/routs/orderRoute.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter ,Depends
+from typing import Optional
+from fastapi import APIRouter ,Depends, HTTPException
 from src.schemas.orderSchema import OrderResponseSchema, OrderCreateSchema
 from src.database import get_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.models.orderModel import OrderModel
+from sqlalchemy.future import select
 
 router = APIRouter(
     prefix = "/orderInfo",
@@ -20,9 +22,31 @@ async def create_order_handler(order: OrderCreateSchema , session: AsyncSession 
         return new_order
 
 
-@router.get("/")
-async def get_order_handler():
-        return await None
+@router.get("/", response_model=list[OrderResponseSchema])
+async def get_order_handler(
+     skip: int = 0,
+     limit: int = 10,
+     user_id: Optional[str] = None,
+     product_id: Optional[str] = None,
+     session: AsyncSession = Depends(get_session)):
+    try:
+
+        query = select(OrderModel).offset(skip).limit(limit)
+        if user_id:
+            query = query.where(OrderModel.user_id == user_id)
+        if product_id:    
+            query = query.where(OrderModel.product_id == product_id)
+        result = await session.execute(query)
+        orders = result.scalars().all()
+        
+        if not orders:
+            raise HTTPException(status_code=404, detail="Orders not found")
+        
+        return orders  
+        
+    except Exception as e:
+        print(f"Error fetching orders: {str(e)}")
+        raise HTTPException(status_code=500, detail="Internal server error")
 
 
 @router.put("/")
