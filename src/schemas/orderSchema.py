6cec8dc31d89c8a4086c925a66895bf28574ac3d
Artem <rapid9791@gmail.com>
2025-08-03 16:26:55 +0300

    feat: added update schema

--- src/schemas/orderSchema.py
+++ src/schemas/orderSchema.py
@@ -35,4 +35,20 @@ class OrderResponseSchema(BaseModel):
     notes: Optional[str] = None
 
     class Config:
-        from_attributes = True   
+        from_attributes = True
+
+class OrderUpdateSchema(BaseModel):
+    user_id: Optional[str] = None
+    product_id: Optional[str] = None
+    quantity: Optional[int] = None
+    order_date: Optional[str] = None
+    delivery_date: Optional[str] = None
+    status: Optional[bool] = None
+    payment_method: Optional[str] = None
+    payment_status: Optional[str] = None
+    shipping_address: Optional[str] = None
+    tracking_number: Optional[str] = None
+    discount_applied: Optional[int] = None
+    total_amount: Optional[int] = None  
+    tax_amount: Optional[int] = None    
+    notes: Optional[str] = None
